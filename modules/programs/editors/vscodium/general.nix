333b23bbb5f94e01f9596b3fe610efd37ed35120
Krezh <krezh@users.noreply.github.com>
2026-05-12 19:33:28 +0200

    another

--- modules/programs/editors/vscodium/general.nix
+++ modules/programs/editors/vscodium/general.nix
@@ -59,6 +59,7 @@
             "christian-kohler.path-intellisense"
             "helm-ls.helm-ls"
             "ms-kubernetes-tools.vscode-kubernetes-tools"
+            "gitHub.vscode-pull-request-github"
           ];
           userSettings = {
             telemetry.telemetryLevel = "off";
