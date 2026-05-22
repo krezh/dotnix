cca047ce03f5758c6814641d35a837b1630e5710
Krezh <krezh@users.noreply.github.com>
2026-05-22 20:56:51 +0200

    improve nixd completion

--- modules/programs/dev/editors/vscodium/lang/nix.nix
+++ modules/programs/dev/editors/vscodium/lang/nix.nix
@@ -1,11 +1,7 @@
 { inputs, ... }:
 {
   flake.modules.homeManager.editors =
-    {
-      lib,
-      pkgs,
-      ...
-    }:
+    { lib, pkgs, ... }:
     {
       vscodium.extensionIds = [
         "jnoortheen.nix-ide"
@@ -36,14 +32,26 @@
                       (let
                         pkgs = ${nixpkgs.expr};
                         lib = import ${inputs.home-manager}/modules/lib/stdlib-extended.nix pkgs.lib;
+                        flake = builtins.getFlake (toString ./.);
                       in (lib.evalModules {
-                        modules = (import ${inputs.home-manager}/modules/modules.nix) {
-                          inherit lib pkgs;
-                          check = false;
-                        };
+                        modules =
+                          ((import ${inputs.home-manager}/modules/modules.nix) {
+                            inherit lib pkgs;
+                            check = false;
+                          })
+                          ++ (builtins.attrValues flake.modules.homeManager)
+                          ++ [
+                            {
+                              _module.args = {
+                                inherit pkgs;
+                                osConfig.system.stateVersion = "24.05";
+                              };
+                            }
+                          ];
                       })).options
                     '';
                     flake_parts.expr = "let flake = builtins.getFlake (toString ./.); in flake.debug.options // flake.currentSystem.options";
+
                   };
                   diagnostic.suppress = [ "sema-extra-with" ];
                   hiddenLanguageServerErrors = [
