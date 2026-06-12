aef9fbac63081fdf45c3cd865f421e6720c157c6
Krezh <17014764+krezh@users.noreply.github.com>
2026-06-13 01:47:03 +0200

    fix(flate): update 0.1.28 -> 0.4.5 and add v prefix to tag (#1481)
    Co-authored-by: Krezh <krezh@users.noreply.github.com>

--- pkgs/bin/flate/default.nix
+++ pkgs/bin/flate/default.nix
@@ -10,18 +10,18 @@
 (buildGoModule.override { go = go-bin.latestStable; }) (finalAttrs: {
   pname = "flate";
   # renovate: datasource=github-releases depName=home-operations/flate
-  version = "0.1.28";
+  version = "0.4.5";
 
   src = fetchFromGitHub {
     owner = "home-operations";
     repo = "flate";
-    tag = finalAttrs.version;
-    hash = "sha256-sA9LNpaJb3HbC8B72ax6Z/sBtc44hU5qwARqFWc3zFw=";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-zaaXHKGuxPrGGas4lRykg3KwzLvyNC1C1lcrwx9xmWE=";
   };
 
   doCheck = false;
 
-  vendorHash = "sha256-HPEIRFf7fBOcB52AL9GlEVe+Tpdqjj89ubO3uPaFQzw=";
+  vendorHash = "sha256-y3C3AqZvQM5dHSUEStRjJvIbCWD6HeSH8v8j4NMn7jg=";
 
   ldflags = [
     "-s"
