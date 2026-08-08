c269a9556077e58071c80da6c05fcd8079108596
Krezh <krezh@users.noreply.github.com>
2026-08-08 04:46:46 +0200

    update

--- overlays/weston-fix.nix
+++
@@ -1,7 +0,0 @@
-# Weston overlay to fix DRM format modifier assertion failure on AMD GPUs
-# This fixes the crash: "Assertion `!weston_drm_format_has_modifier(format, modifier)' failed"
-_final: prev: {
-  weston = prev.weston.overrideAttrs (oldAttrs: {
-    patches = (oldAttrs.patches or [ ]) ++ [ ./fix-duplicate-modifiers.patch ];
-  });
-}
